97d270883bcb0f4ec74f94ed8b1aff04dd7352c2
Harsha1031 <sriharsha1031@gmail.com>
2023-07-27 15:18:18 +0530

    Fixed result.URL.length undefined issue

--- Logic.js
+++ Logic.js
@@ -3,11 +3,13 @@ let loca = String(window.location.href);
 setTimeout(() => {
 
     chrome.storage.local.get(["URL"]).then((result) => {
-        for (let i = 0; i < result.URL.length; i++) {
-            if (loca.includes(result.URL[i])) {
-                var body = document.getElementsByTagName("body")[0];
-                body.style.backgroundColor = "white";
-                body.innerHTML = `<h1> You are trying to access ${result.URL[i]}. Which is blocked website. </h1>`;
+        if (result.URL.length) {
+            for (let i = 0; i < result.URL.length; i++) {
+                if (loca.includes(result.URL[i])) {
+                    var body = document.getElementsByTagName("body")[0];
+                    body.style.backgroundColor = "white";
+                    body.innerHTML = `<h1> You are trying to access ${result.URL[i]}. Which is blocked website. </h1>`;
+                }
             }
         }
     });
